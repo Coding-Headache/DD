ec4117a53e5ee6217280d4d1915be71b87d4b17d
Daniel Hyde <dan.hyde@acodingheadache.com>
2016-10-27 03:05:47 +0100

    started calc.vhd

--- wk3/part2/calculator.vhd
+++ wk3/part2/calculator.vhd
@@ -9,3 +9,33 @@
 --					 produce an additional math equation, passing the results to the
 --					 7-segment display.
 ---------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity calculator is
+port(A, B: in std_logic_vector(3 downto 0);
+     D1  : out std_logic_vector(7 downto 0);
+	  D0  : out std_logic_vector(7 downto 0)
+	  );
+end entity;
+
+architecture structural of calculator is
+
+component four_bit_adder
+port(A, B: in std_logic_vector(3 downto 0);
+     Q   : out std_logic_vector(3 downto 0);
+	  Cout: out std_logic);
+end component;
+component circuitA
+port(Sin : in std_logic_vector(3 downto 0);
+     Cout: in std_logic;
+	  D1  : out std_logic_vector(7 downto 0);
+	  D0  : out std_logic_vector(7 downto 0)
+	  );
+end component;
+signal D : std_logic_vector(4 downto 0);
+
+begin
+
+
+end architecture;
